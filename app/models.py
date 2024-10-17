9a5df1fc86c5ac354f9722a72179b43dc9844b4d
CinnamonPie <vonahkahs.s.ved@gmail.com>
2024-10-17 14:43:13 +0400

    litle change in models

--- app/models.py
+++ app/models.py
@@ -23,7 +23,7 @@ class Message(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     content = Column(Text)
-    timestamp = Column(DateTime, default=datetime.utcnow)
+    timestamp = Column(DateTime, default=datetime.now())
     sender_id = Column(Integer, ForeignKey("users.id"))
     receiver_id = Column(Integer, ForeignKey("users.id"))
 
