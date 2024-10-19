b8a03540d03a8d55e6420630369cd39683a2181c
CinnamonPie <vonahkahs.s.ved@gmail.com>
2024-10-19 11:36:15 +0400

    some change in alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,10 +5,6 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.config import settings
-from src.models import User
-from src.database import Base
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,14 +14,11 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option(
-    "sqlalchemy.url", settings.DATABASE_URL_asyncpg + "?async_fallback=True")
-
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -72,9 +65,7 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            compare_server_default=True,
+            connection=connection, target_metadata=target_metadata
         )
 
         with context.begin_transaction():
